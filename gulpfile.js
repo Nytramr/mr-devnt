52bdecfcf974ad6c331266992abfba827501b3ca
Martin Rubinsztein <martin.rubinsztein@gmail.com>
2015-09-04 09:31:33 -0300

    Implementing uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 var browserSync = require('browser-sync').create();
+
+//Uglify
+gulp.task('compress', function() {
+  return gulp.src('src/*.js')
+    .pipe(uglify())
+    .pipe(rename({
+      extname: '.min.js'
+    }))
+    .pipe(gulp.dest('dist'));
+});
 
 // Static server
 gulp.task('browser-sync', function() {
@@ -9,5 +21,7 @@ gulp.task('browser-sync', function() {
         }
     });
 });
+
+gulp.task('build', ['compress']);
 
 gulp.task('default', ['browser-sync']);
